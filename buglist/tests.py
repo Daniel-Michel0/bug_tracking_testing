f1880495d7f0278f9f05b5f131d384804e40f358
miosv <mvilla2019@inf.udec.cl>
2023-10-02 04:31:49 -0300

    ✅ Setup test buglist
    Se dejó más simple por un error con los id's de usuario

--- buglist/tests.py
+++ buglist/tests.py
@@ -1,38 +1,49 @@
 from django.test import TestCase
 from django.urls import reverse
 from django.contrib.auth.models import User
-from database.models import Proyecto, ReporteBug, Bug
-
-# Create your tests here.
+from database.models import Proyecto, ReporteBug, Bug, Programador, Usuario
+from django.contrib.auth import get_user_model
 
 class BuglistTestCase(TestCase):
 
     def setUp(self):
-        # Crea 5 proyectos de muestra
-        self.proyectos = []
-        for i in range(1, 6):
-            proyecto = Proyecto.objects.create(nombre_proyecto=f'Proyecto de prueba {i}')
-            self.proyectos.append(proyecto)
+        self.username = "testuser"
+        self.password = "testpassword123"
+        self.email = "test@email.com"
 
-        # Crea 25 reportes de bugs de muestra, asignando 5 reportes a cada proyecto
-        reportes = []
-        for i in range(1, 26):
-            proyecto = self.proyectos[(i - 1) % 5]  # Asigna 5 reportes a cada proyecto en un ciclo
-            reporte = ReporteBug.objects.create(
-                titulo=f'Reporte de prueba {i}',
-                fecha_reporte='2023-10-01',
-                id_proyecto=proyecto
-            )
-            reportes.append(reporte)
+        self.user = get_user_model().objects.create_user(
+            self.username,
+            self.email,
+            self.password
+        )
 
-        # Crea 5 bugs de muestra, asignando 1 bug a cada proyecto
-        for i in range(1, 6):
-            proyecto = self.proyectos[i - 1]
-            bug = Bug.objects.create(
-                titulo=f'Bug de prueba {i}',
-                fecha_reporte='2023-10-01',
-                estado='ASIGNADO',
-                id_proyecto=proyecto
-            )
+        self.programador = Programador.objects.create(
+            user=self.user,
+        )
 
-    
+        self.usuario = Usuario.objects.create(
+            user=self.user,
+        )
+
+        self.proyecto = Proyecto.objects.create(
+            nombre_proyecto="Proyecto de prueba"
+        )
+
+        self.reporte = ReporteBug.objects.create(
+            id_proyecto=self.proyecto,
+            titulo="Reporte de prueba",
+            reporte="Este es un reporte de prueba",
+            fecha_reporte="2020-01-01",
+            estado="Pendiente",
+            id_usuario = self.usuario
+        )
+
+        self.bug = Bug.objects.create(
+            id_reporte=self.reporte,
+            titulo="Bug de prueba",
+            descripcion="Este es un bug de prueba",
+            fecha_reporte="2020-01-01",
+            estado="Pendiente",
+            id_programador=self.programador
+        )
+
