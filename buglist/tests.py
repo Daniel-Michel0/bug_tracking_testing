f391c1510773fd4238bdca9dba7135728450a90e
miosv <mvilla2019@inf.udec.cl>
2023-10-02 19:34:19 -0300

    ✅ Agregado test de paginacion en buglist
    Se agrego 1 test para probar una pagina existente y otro para una pagina invalida

--- buglist/tests.py
+++ buglist/tests.py
@@ -3,6 +3,7 @@ from django.urls import reverse
 from django.contrib.auth.models import User
 from database.models import Proyecto, ReporteBug, Bug, Programador, Usuario
 from django.contrib.auth import get_user_model
+import re
 
 class BuglistTestCase(TestCase):
 
@@ -56,6 +57,15 @@ class BuglistTestCase(TestCase):
                 id_proyecto=proyecto,
                 id_programador=programador,
             )
+
+        # Creo otro bug para tener dos paginas en la paginacion
+        Bug.objects.create(
+            titulo=f'Bug de prueba 6',
+            fecha_reporte='2023-10-01',
+            estado='ASIGNADO',
+            id_proyecto=proyectos[0],
+            id_programador=programadores[0],
+        )
     
     def test_view_normal(self):
         # Prueba que la vista normal se carga correctamente
@@ -74,3 +84,31 @@ class BuglistTestCase(TestCase):
         response = self.client.get(reverse('buglist:report_list_pagination'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'buglist/buglist.html')
+
+    def test_buglist_view_pagination_page(self):
+        # Prueba que la vista buglist se carga correctamente en la página 2
+        response = self.client.get(reverse('buglist:bug_list_pagination') + '?bug_page=2')
+        self.assertEqual(response.status_code, 200)
+        
+        # Extraer el contenido HTML de la respuesta
+        content = response.content.decode('utf-8')  # Decodificar el contenido a una cadena UTF-8
+
+        # Usar una expresión regular para encontrar el número de página en la clase "current-page"
+        match = re.search(r'class="current-page">(\d+)</span>', content)
+    
+        # Verificar que el número de página en el contenido de la respuesta sea el número de página correcto (en este caso, 2)
+        self.assertEqual(match.group(1), '2')
+
+    def test_buglist_view_pagination_page_invalid(self):
+        # Prueba que la vista buglist muestre la última página válida cuando se ingresa un número de página inválido
+        response = self.client.get(reverse('buglist:bug_list_pagination') + '?bug_page=999')
+        self.assertEqual(response.status_code, 200)  # Verificar que la respuesta sea exitosa (código 200)
+        
+        # Extraer el contenido HTML de la respuesta
+        content = response.content.decode('utf-8')
+
+        # Usar una expresión regular para encontrar el número de página en la clase "current-page"
+        match = re.search(r'class="current-page">(\d+)</span>', content)
+
+        # Verificar que el número de página sea el de la ultima pagina valida (en este caso, 2)
+        self.assertEqual(match.group(1), '2')
