947b20501b16837b8dd0a22408acfc6c4020b330
Daniel Michel <timydan@gmail.com>
2023-11-15 21:44:10 -0300

    ✅ Added run_integration_tests.py

---
+++ run_integration_tests.py
@@ -0,0 +1,14 @@
+import subprocess
+
+#* Python path
+python_path = ".\.venv\Scripts\python.exe"
+
+def run_tests():
+    print("Running register_test.py...")
+    subprocess.run([".\.venv\Scripts\python.exe", "home/register_test.py"], check=True)
+
+    print("Running login_send_bug_test.py...")
+    subprocess.run([".\.venv\Scripts\python.exe", "home/login_send_bug_test.py"], check=True)
+
+if __name__ == "__main__":
+    run_tests()
